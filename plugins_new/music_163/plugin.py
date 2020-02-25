679af7f28d4cfcf2805deae55a0636f051f003c7
Antares <antares.oier@gmail.com>
2020-02-26 02:35:22 +0800

    fix bugs

--- plugins_new/music_163/plugin.py
+++ plugins_new/music_163/plugin.py
@@ -84,7 +84,8 @@ class Music163Plugin(Plugin):
             del args[-1]
 
         if not await self.check_login_status():
-            if not await self.login():
+            await self.login()
+            if not await self.check_login_status():
                 await self.bot.client_async.send("网易云账号登陆失败！")
 
         if args[0] == "id":
