0c697dc89e535f3ca2f8e4c16221c934ce58b83c
officeyutong <i@ytbolg.net>
2020-02-25 11:52:08 +0800

    qwq

--- common/api_client/api.py
+++ common/api_client/api.py
@@ -8,32 +8,33 @@ class ClientWrapper:
 
     def send_private_msg(self, user_id: int, message: Union[str, dict], auto_escape: bool = False) -> int:
         local_vars = locals()
-        local_vars.popitem("self")
+        del local_vars["self"]
         return self.invoker("send_private_msg", local_vars)
 
     def send_group_msg(self, group_id: int, message: Union[str, dict], auto_escape: bool = False) -> int:
         local_vars = locals()
-        local_vars.popitem("self")
+        del local_vars["self"]
         return self.invoker("send_group_msg", local_vars)
 
     def send_discuss_msg(self, discuss_id: int, message: Union[str, dict], auto_escape: bool = False) -> int:
         local_vars = locals()
-        local_vars.popitem("self")
+        del local_vars["self"]
         return self.invoker("send_discuss_msg", local_vars)
 
     def send_msg(self, message: Union[str, dict], message_type: str = "", discuss_id: int = -1, user_id: int = -1, group_id: int = -1, auto_escape: bool = False) -> int:
         local_vars = locals()
-        local_vars.popitem("self")
+        del local_vars["self"]
         return self.invoker("send_msg", local_vars)
 
     def delete_msg(self, message_id: int):
         local_vars = locals()
-        local_vars.popitem("self")
+        del local_vars["self"]
         return self.invoker("delete_msg", local_vars)
 
     def send_like(self, user_id: int, times: int = -1):
         local_vars = locals()
-        local_vars.popitem("self")
+        # del local_vars["self"]
+        del local_vars["self"]
         return self.invoker("send_like", local_vars)
 
     def send(self, context: dict, message: Union[str, dict], auto_escape: bool = False):
