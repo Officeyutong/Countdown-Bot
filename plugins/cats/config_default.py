cf9ace753daba56fdd03c7aac588d88f43aadb9b
officeyutong <i@ytbolg.net>
2020-02-26 21:57:26 +0800

    qwq

--- plugins/cats/config_default.py
+++
@@ -1,6 +0,0 @@
-ACCESS_KEY_ID = "qwq"
-ACCESS_KEY_SECRET = "qwq"
-ENDPOINT = "http://oss-cn-hangzhou.aliyuncs.com"
-BUCKET_NAME = "qwq"
-IMAGE_SIZE_LIMIT = 8*1024*1024
-WHITE_LIST_GROUPS = ["123456"]
