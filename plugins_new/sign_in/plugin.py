1fc2c788adeb8aeb1771a00de589d03c661012da
Antares <antares.oier@gmail.com>
2020-02-28 16:02:49 +0800

    添加查询接口，增加用户查询积分指令

--- plugins_new/sign_in/plugin.py
+++ plugins_new/sign_in/plugin.py
@@ -4,9 +4,10 @@ from common.datatypes import PluginMeta
 from common.countdown_bot import CountdownBot
 from common.loop import TimeTuple
 from common.command import ChatType
-from common.event import GroupMessageEvent
+from common.event import GroupMessageEvent,PrivateMessageEvent
 from typing import List, Dict
-from .datatypes import SignInData
+from io import StringIO
+from .datatypes import *
 
 import sqlite3
 import time
@@ -19,34 +20,100 @@ class SignInConfig(ConfigBase):
 
 class SignInPlugin(Plugin):
     def get_last_sign_in_data(self, group_id: int, user_id: int) -> SignInData:
+        """
+        返回某群某人上一次的签到记录
+        若不存在，返回均为参数0的初始数据
+        @param group_id: 群号
+        @param user_id: QQ号
+        """
         if self.conn.execute(
-                "SELECT COUNT(*) FROM SIGNINS WHERE GROUP_ID = ? AND USER_ID = ?", 
+                "SELECT COUNT(*) FROM SIGNINS WHERE GROUP_ID = ? AND USER_ID = ?",
                 (group_id, user_id,)).fetchone()[0]:
             # 存在上一条记录
             last_time: int = self.conn.execute(
-                "SELECT MAX(TIME) FROM SIGNINS WHERE GROUP_ID = ? AND USER_ID = ?", 
-                (group_id, user_id,)).fetchone()[0] # 得到上次签到时间
+                "SELECT MAX(TIME) FROM SIGNINS WHERE GROUP_ID = ? AND USER_ID = ?",
+                (group_id, user_id,)).fetchone()[0]  # 得到上次签到时间
             result = self.conn.execute(
                 "SELECT * FROM SIGNINS WHERE GROUP_ID = ? AND USER_ID = ? AND TIME = ?", (
                     group_id,
                     user_id,
                     last_time,
-                )).fetchone() #上次签到数据
+                )).fetchone()  # 上次签到数据
             return SignInData(result[0], result[1], result[2], result[3], result[4], result[5])
         else:
             return SignInData(group_id, user_id, 0, 0, 0, 0)
 
     def clac_sign_in_times(self, group_id: int, user_id: int) -> (int, int):
+        """
+        返回两个int值，表示某群某人总签到次数和当前月份签到次数
+        @param group_id: 群号
+        @param user_id: QQ号
+        """
         now = time.localtime(int(time.time()))
         this_month = int(time.mktime(time.strptime(
-            f"{now.tm_year}-{now.tm_mon}", "%Y-%m"))) # 获得当前月开始的时间戳
+            f"{now.tm_year}-{now.tm_mon}", "%Y-%m")))  # 获得当前月开始的时间戳
         all_times = self.conn.execute(
-            "SELECT COUNT(*) FROM SIGNINS WHERE GROUP_ID = ? AND USER_ID = ?", 
-            (group_id, user_id,)).fetchone()[0] # 签到总数
+            "SELECT COUNT(*) FROM SIGNINS WHERE GROUP_ID = ? AND USER_ID = ?",
+            (group_id, user_id,)).fetchone()[0]  # 签到总数
         month_times = self.conn.execute(
             "SELECT COUNT(*) FROM SIGNINS WHERE GROUP_ID = ? AND USER_ID = ? AND TIME >= ? ",
-            (group_id, user_id, this_month,)).fetchone()[0] #本月签到数
+            (group_id, user_id, this_month,)).fetchone()[0]  # 本月签到数
         return all_times, month_times
+
+    def get_user_data(self, user_id: int) -> List[UserData]:
+        """
+        返回某人在各群的签到数据
+        @param user_id: QQ号
+        """
+        result = self.conn.execute(
+            "SELECT * FROM USERS WHERE USER_ID = ?", (user_id,)).fetchall()
+        data: List[UserData] = list(
+            UserData(row[0], row[1], row[2]) for row in result)
+        return data
+
+    def get_group_sign_in_ranklist(self, group_id: int) -> List[UserData]:
+        """
+        返回某群签到排名列表，按照积分降序排列
+        @param group_id: 群号
+        @param user_id: QQ号
+        """
+        result = self.conn.execute("SELECT * FROM USERS WHERE GROUP_ID = ?", (
+            group_id,
+        )).fetchall()
+
+        data: List[UserData] = list(
+            UserData(row[0], row[1], row[2]) for row in result)
+
+        return sorted(data, key=lambda x: x.score, reverse=True)
+
+    def get_sign_in_data(self, time_begin: int, time_end: int, group_id: int, user_id: int = 0) -> List[SignInData]:
+        """
+        返回某个时间段内某群(某人)的签到记录
+        @param time_begin: 开始时间戳
+        @param time_end: 结束时间戳
+        @param group_id: 群号
+        @param user_id: QQ号(可选)
+        """
+        if user_id:
+            result = self.conn.execute(
+                "SELECT * FROM SIGNINS WHERE GROUP_ID = ? AND USER_ID = ? AND TIME >= ? AND TIME <= ?", (
+                    group_id,
+                    user_id,
+                    time_begin,
+                    time_end,
+                )
+            ).fetchall()
+        else:
+            result = self.conn.execute(
+                "SELECT * FROM SIGNINS WHERE GROUP_ID = ? AND TIME >= ? AND TIME <= ?", (
+                    group_id,
+                    time_begin,
+                    time_end,
+                )
+            ).fetchall()
+        data: List[SignInData] = list(
+            SignInData(row[0], row[1], row[2], row[3], row[4], row[5]) for row in result)
+        return data
 
     def save_data(self, sign_in_data: SignInData) -> None:
         self.conn.execute(
@@ -57,8 +124,8 @@ class SignInPlugin(Plugin):
                 sign_in_data.duration,
                 sign_in_data.score,
                 sign_in_data.score_changes,
-            )) # 写入签到记录
-        
+            ))  # 写入签到记录
+
         if self.conn.execute("SELECT COUNT(*) FROM USERS WHERE GROUP_ID = ? AND USER_ID = ?",
                              (sign_in_data.group_id, sign_in_data.user_id,)).fetchone()[0]:
             # 检查用户信息存在
@@ -66,28 +133,30 @@ class SignInPlugin(Plugin):
                 sign_in_data.score,
                 sign_in_data.group_id,
                 sign_in_data.user_id,
-            )) #更新用户信息
+            ))  # 更新用户信息
         else:
             self.conn.execute("INSERT INTO USERS (GROUP_ID,USER_ID,SCORE) VALUES (?,?,?)", (
                 sign_in_data.group_id,
                 sign_in_data.user_id,
                 sign_in_data.score,
-            )) # 新建用户信息
+            ))  # 新建用户信息
         self.conn.commit()
 
     def command_sign_in(self, plugin, args: List[str], raw_string: str, context: dict, evt: GroupMessageEvent):
-        group_id = int(context['group_id'])
+        group_id = int(evt.group_id)
 
         if str(group_id) in self.config.BLACK_LIST_GROUPS:
             self.bot.client_async.send(context, "签到功能在本群停用")
             return
 
-        user_id = int(context['sender']['user_id'])
+        user_id = int(evt.sender.user_id)
 
-        last_sign_in_data = self.get_last_sign_in_data(group_id, user_id) #上次签到的数据
-        last_time = time.localtime(last_sign_in_data.time) #上次签到的时间
-        current_time = time.localtime(int(time.time())) #当前时间
-        all_times, month_times = self.clac_sign_in_times(group_id, user_id) #总签到次数，当月签到次数
+        last_sign_in_data = self.get_last_sign_in_data(
+            group_id, user_id)  # 上次签到的数据
+        last_time = time.localtime(last_sign_in_data.time)  # 上次签到的时间
+        current_time = time.localtime(int(time.time()))  # 当前时间
+        all_times, month_times = self.clac_sign_in_times(
+            group_id, user_id)  # 总签到次数，当月签到次数
 
         if last_time.tm_year == current_time.tm_year and last_time.tm_yday == current_time.tm_yday:
             # 年份和天数都相等，当天已经签到
@@ -100,14 +169,14 @@ class SignInPlugin(Plugin):
 
         sign_in_data = SignInData
 
-        sign_in_data.time = int(time.mktime(current_time)) # 签到时间
+        sign_in_data.time = int(time.mktime(current_time))  # 签到时间
         sign_in_data.user_id = user_id
         sign_in_data.group_id = group_id
 
         if last_time.tm_year == current_time.tm_year:
             # 上次签到和本次签到年份相等
             if last_time.tm_yday + 1 == current_time.tm_yday:
-                #上次签到日期和本次差1，连续签到
+                # 上次签到日期和本次差1，连续签到
                 sign_in_data.duration = last_sign_in_data.duration+1
             else:
                 sign_in_data.duration = 1
@@ -132,9 +201,9 @@ class SignInPlugin(Plugin):
         # 10-30天：10
         # 大于30天：15
 
-        sign_in_data.score_changes = 10 + duration_add #基础积分：10
+        sign_in_data.score_changes = 10 + duration_add  # 基础积分：10
         sign_in_data.score = last_sign_in_data.score + sign_in_data.score_changes
-        
+
         self.save_data(sign_in_data)
 
         self.bot.client_async.send(context,
@@ -145,6 +214,15 @@ class SignInPlugin(Plugin):
 当前积分：{sign_in_data.score}
 本月签到次数：{month_times+1}
 累计群签到次数：{all_times+1}""")
+
+    def command_user_query(self, plugin, args: List[str], raw_string: str, context: dict, evt: PrivateMessageEvent):
+        user_id = int(evt.sender.user_id)
+        user_data = self.get_user_data(user_id)
+        buf = StringIO()
+        buf.write(f"查询到您在{len(user_data)}个群有签到记录:\n")
+        for item in user_data:
+            buf.write(f"群{item.group_id}积分为：{item.score}\n")
+        self.bot.client_async.send(context, buf.getvalue())
 
     def on_disable(self):
         self.logger.info(f"SignIn: closing database..")
@@ -187,7 +265,14 @@ class SignInPlugin(Plugin):
             command_handler=self.command_sign_in,
             help_string="签到",
             chats={ChatType.group},
-            alias=["sign-in", "check-in"],
+            alias=["sign-in", "check-in"]
+        )
+        self.register_command_wrapped(
+            command_name="签到积分",
+            command_handler=self.command_user_query,
+            help_string="签到积分查询",
+            chats={ChatType.private},
+            alias=["myscore"]
         )
 
 
