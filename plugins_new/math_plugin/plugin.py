e8946343efa5577fd9bc4d3c0a19abdea191899e
officeyutong <i@ytbolg.net>
2020-03-14 17:14:58 +0800

    latex fix

--- plugins_new/math_plugin/plugin.py
+++ plugins_new/math_plugin/plugin.py
@@ -271,8 +271,7 @@ LaTeX:
             raise ex
 
     async def command_render_latex(self, plugin, args: List[str], raw_string: str, context: dict, evt: MessageEvent):
-
-        to_render = base64.encodebytes(raw_string.replace(
+        to_render = base64.encodebytes(html.unescape(raw_string).replace(
             "latex ", "", 1).encode()).decode().replace("\n", "")
         TEMPLATE = "import base64\noutput={'latex':'','python_expr':'','image':render_latex(base64.decodebytes('{to_render}'.encode()).decode())}".replace(
             "to_render", to_render)
