eb667ab43f06438b90ce0f6358c4af5ee1b53894
Antares <antares.oier@gmail.com>
2020-04-01 16:50:36 +0800

    签到排名查询接口

--- plugins_new/sign_in/datatypes.py
+++ plugins_new/sign_in/datatypes.py
@@ -16,3 +16,13 @@ class UserData:
     group_id: int
     user_id: int
     score: int = 0
+
+
+@dataclass
+class RanklistItem:
+    group_id: int
+    user_id: int
+    score: int
+    last_time: int
+    month_times: int
+    total_times: int
